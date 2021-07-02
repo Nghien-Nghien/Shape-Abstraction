1477609b683d3d5b0b0e3a83d1098fb34044e30a
Nghien-Nghien <nguyenhoangsang28091995@gmail.com>
2021-07-02 14:13:27 +0700

    final commit

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,14 @@ import java.util.ArrayList;
 
 public class Main {
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         System.out.println("This is program use to calculate Circuit n Area of Shapes:");
-        ArrayList <Shape> shape = new ArrayList <>();
+        ArrayList<Shape> shapes = new ArrayList<>();
 
-        shape.add(new Square(10.5f));
-        shape.add(new Rectangle(11.0f,15.0f));
-        for (int i = 0 ; i < shape.size() ; i++) {
-            shape.get(i).printInformation();
+        shapes.add(new Square(10.5f));
+        shapes.add(new Rectangle(11.0f, 15.0f));
+        for (int i = 0; i < shapes.size(); i++) {
+            shapes.get(i).printInformation();
         }
     }
 }
